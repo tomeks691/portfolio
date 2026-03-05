83d7b7fc00123630cf9f8d929569e722396b5f57
lucek <luci6969@protonmail.ch>
2026-03-05 10:41:41 +0100

    fix: base path for portfolio repo name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://tomek-mcclintock.github.io',
+  site: 'https://tomeks691.github.io',
   base: '/portfolio',
   integrations: [tailwind()],
 });
