887c03c79c18504082e0ee2e9206cbf35503ad8f
lucek <luci6969@protonmail.ch>
2026-03-06 11:49:42 +0100

    fix: update base path for Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://tomeks691.github.io',
-  base: '/portfolio',
+  site: 'https://portfolio.tomaszsosnowski.pl',
+  base: '/',
   integrations: [tailwind()],
 });
